76a0933897a783218fe2d01774499cfd4af19382
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-11-02 12:38:11 -0700

    fixing betty task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_h = right_h = 0;
 	if (tree == NULL)
 		return (0);
-	if (tree-> left == NULL && tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 	left_h = binary_tree_height(tree->left) + 1;
 	right_h = binary_tree_height(tree->right) + 1;
